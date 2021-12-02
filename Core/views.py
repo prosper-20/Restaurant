6d2504ba2f8bb55bc3ad25aab6e6d595323822cb
prosper-20 <edwardprosper001@gmail.com>
2021-12-02 16:46:10 +0100

    new views

--- Core/views.py
+++ Core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "Core/index.html")
+
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "Core/About.html")
