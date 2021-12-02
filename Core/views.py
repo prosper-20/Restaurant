440b2d90f28bbed4bad54ddd0b8b8f83e0d67435
prosper-20 <edwardprosper001@gmail.com>
2021-12-02 19:47:18 +0100

    added Post View

--- Core/views.py
+++ Core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from .models import MenuItem, OrderModel
+from django.core.mail import send_mail
 
 class Index(View):
     def get(self, request, *args, **kwargs):
@@ -24,7 +25,7 @@ class Order(View):
             'desserts': desserts,
             'drinks': drinks,
         }
-        return render(request, 'customer/order.html', context)
+        return render(request, 'Core/order.html', context)
 
     def post(self, request, *args, **kwargs):
 
@@ -57,5 +58,66 @@ class Order(View):
             'price': price
         }
 
-        return render(request, 'customer/order_confirmation.html', context)
+        return render(request, 'Core/order_confirmation.html', context)
+
+    def post(self, request, *args, **kwargs):
+        # Get input fields at the bottom of the order template
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip')
+
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+            }
+            order_items['items'].append(item_data)
+        
+        price = 0
+        item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+        
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            email=email,
+            street=street,
+            city=city,
+            state=state,
+            zip_code=zip_code
+        )
+        order.items.add(*item_ids)
+
+        # After everything is done, send confirmation email to user
+        body = ('Thank you for your order!  Your food is being made and will be delivered soon!\n'
+        f'Your total: {price}\n'
+        'Thank you again for your order!')
+
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently=False
+        )
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return render(request, 'Core/order_confirmation.html', context)
 
