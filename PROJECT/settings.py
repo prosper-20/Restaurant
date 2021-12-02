6474c15d22273721de5feb0c6e87500f32ec45ea
prosper-20 <edwardprosper001@gmail.com>
2021-12-02 19:47:51 +0100

    Added Email Backend

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -129,3 +129,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
