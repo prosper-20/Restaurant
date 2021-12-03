fe20a1ee0bd8add7cec7b627e938ea7b576a135f
prosper-20 <edwardprosper001@gmail.com>
2021-12-03 11:33:01 +0100

    Updated

--- Core/urls.py
+++ Core/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import Index, About, Order
+from .views import (
+    Index,
+    About,
+    Order,
+    OrderConfirmation,
+    OrderPayConfirmation)
 from . import views
 
 
@@ -7,4 +12,6 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
     path('order/', Order.as_view(), name='order'),
+    path('order-confirmation/<int:pk>/', OrderConfirmation.as_view(), name='order-confirmation'),
+    path('payment-confirmation/', OrderPayConfirmation.as_view(), name='payment-confirmation')
 ]
