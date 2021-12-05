07bb6f9fc18fca618e5e71d246c7b58d7960a9e2
prosper-20 <edwardprosper001@gmail.com>
2021-12-05 21:50:12 +0100

    added list-disply

--- Core/admin.py
+++ Core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import OrderModel, MenuItem, Category
 
-admin.site.register(OrderModel)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_paid']
+
+admin.site.register(OrderModel, OrderAdmin)
 admin.site.register(MenuItem)
 admin.site.register(Category)
