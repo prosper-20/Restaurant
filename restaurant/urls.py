df4b2a6f0dc4c85adb25f213295e5647bcea0415
prosper-20 <edwardprosper001@gmail.com>
2021-12-06 22:43:04 +0100

    updated urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,8 +2,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import Dashboard, OrderDetails
+from Core.views import new_home
+from Core import views
 
 urlpatterns = [
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
-    path('order/<int:pk>/', OrderDetails.as_view(), name='order-details')
+    path('order/<int:pk>/', OrderDetails.as_view(), name='order-details'),
+    path('index/', views.new_home, name="new_home" )
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
