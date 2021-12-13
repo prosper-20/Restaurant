599b5f67b787fa0828330cf8555b2d012ae589ab
prosper-20 <edwardprosper001@gmail.com>
2021-12-13 09:00:24 +0100

    urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -8,6 +8,6 @@ from Core import views
 urlpatterns = [
     path('dashboard/', Dashboard.as_view(), name='dashboard'),
     path('order/<int:pk>/', OrderDetails.as_view(), name='order-details'),
-    path('index/', views.new_home, name="new_home" )
+    path('index/', views.new_home, name="new_home" ),
     path('clone/', views.clone_new_home, name="clone_home")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
