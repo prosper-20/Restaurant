03f4bf660e81ca677cf08d383e5c10583ecc9ab8
prosper-20 <edwardprosper001@gmail.com>
2021-12-14 06:39:58 +0100

    urls

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from users import views as users_views
+from users.views import register
+
+
+urlpatterns = [
+    path('', users_views.register, name='register'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
