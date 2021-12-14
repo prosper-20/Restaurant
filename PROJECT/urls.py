b91ab0ee889a22ef99182e438a1397d14efffb8d
prosper-20 <edwardprosper001@gmail.com>
2021-12-14 08:10:00 +0100

    further

--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -19,7 +19,7 @@ from django.conf.urls.static import static
 from django.urls import path, include
 from Core.views import Menu, MenuSearch
 from users import views as user_views
-from users.views import register
+from users.views import register, profile_edit
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth import views as auth_views
 
@@ -35,6 +35,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name='logout'),
     path('profile/', user_views.profile, name='profile'),
+    path("profile/edit/", user_views.profile_edit, name="profile_edit"),
+
 ]
 
 if settings.DEBUG:
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import fields
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -11,5 +13,24 @@ class UserRegisterForm(UserCreationForm):
 
 
         fields = ["username", 'email', "password1", "password2"]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(help_text="Enter a valid email address")
+
+    class Meta:
+        model = User
+
+
+        fields = ["username", 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+
+
+        fields = ["image"]
+
 
         
