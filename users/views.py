6377ac9f712e9b802d8cfab09fe11a6606281ad9
prosper-20 <edwardprosper001@gmail.com>
2021-12-14 05:52:06 +0100

    needed views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            messages.success(request, f"Hi {username}, your account has been created successfully!")
+            return redirect("new_home")
+    else:
+        form = UserCreationForm()
+    return render(request, "users/register.html", {"form": form})
+
