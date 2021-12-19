8c9bff3b4eb5fc95cc7dd10497cdfdc423f3cc0d
prosper-20 <edwardprosper001@gmail.com>
2021-12-19 15:47:26 +0100

    updates

--- Core/urls.py
+++ Core/urls.py
@@ -3,6 +3,7 @@ from .views import (
     Index,
     About,
     Order,
+    Test_Order,
     OrderConfirmation,
     OrderPayConfirmation)
 from . import views
@@ -12,6 +13,7 @@ urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('about/', About.as_view(), name='about'),
     path('order/', Order.as_view(), name='order'),
+    path('test-order/', Test_Order.as_view(), name="test_order"),
     path('order-confirmation/<int:pk>/', OrderConfirmation.as_view(), name='order-confirmation'),
     path('payment-confirmation/', OrderPayConfirmation.as_view(), name='payment-confirmation'),
     
--- Core/views.py
+++ Core/views.py
@@ -8,6 +8,118 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 import json
 from django.db.models import Q
 
+class Test_Order(View):
+    
+    def get(self, request, *args, **kwargs):
+        appetizers = MenuItem.objects.filter(category__name__contains='Appetizer')
+        entres = MenuItem.objects.filter(category__name__contains='Entre')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+        context = {
+            'appetizers': appetizers,
+            'entres': entres,
+            'desserts': desserts,
+            'drinks': drinks,
+        }
+        return render(request, 'Core/clone_2.html', context)
+
+    def post(self, request, *args, **kwargs):
+
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+            }
+            order_items['items'].append(item_data)
+        
+        price = 0
+        item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+        
+        order = OrderModel.objects.create(price=price)
+        order.items.add(*item_ids)
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return render(request, 'Core/order_confirmation.html', context)
+
+    def post(self, request, *args, **kwargs):
+        # Get input fields at the bottom of the order template
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip')
+
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+            }
+            order_items['items'].append(item_data)
+        
+        price = 0
+        item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+        
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            email=email,
+            street=street,
+            city=city,
+            state=state,
+            zip_code=zip_code
+        )
+        order.items.add(*item_ids)
+
+        # After everything is done, send confirmation email to user
+        body = ('Thank you for your order!  Your food is being made and will be delivered soon!\n'
+        f'Your total: {price}\n'
+        'Thank you again for your order!')
+
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently=False
+        )
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return redirect('order-confirmation', pk=order.pk)
+
+
+# THIS IS FOR THE BOOTSTRAP TEMPLATE
 def home_page(request):
     return render(request, 'Core/home-page.html')
 
