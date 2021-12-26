aece38da8b318c9189281f304a2564f114579533
prosper-20 <edwardprosper001@gmail.com>
2021-12-26 22:20:07 +0100

    updates

--- Order/urls.py
+++ Order/urls.py
@@ -3,7 +3,8 @@ from .views import (
     HomeView,
     ItemDetailView,
     add_to_cart, remove_from_cart,
-    OrderSummaryView
+    OrderSummaryView,
+    remove_single_item_from_cart
 )
 from . import views
 
@@ -13,5 +14,6 @@ urlpatterns = [
     path('product/<slug>/', ItemDetailView.as_view(), name='product'),
     path('add-to-cart/<slug>/', add_to_cart, name='add_to_cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name='remove_from_cart'),
-    path('order-summary/', OrderSummaryView.as_view(), name="order_summary")
+    path('order-summary/', OrderSummaryView.as_view(), name="order_summary"),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove_single_item_from_cart'),
 ]
--- Order/views.py
+++ Order/views.py
@@ -89,6 +89,35 @@ def remove_from_cart(request, slug):
     return redirect('product', slug=slug)
 
 
+@login_required
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+         ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        #check if the orderitem is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            order_item.quantity -= 1
+            order_item.save()
+            messages.info(request, "This item quantity was updated ")
+            return redirect('order-summary')
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect('product', slug=slug)
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("product", slug=slug)
+    return redirect('product', slug=slug)
+
+
 
 
 def product(request):
