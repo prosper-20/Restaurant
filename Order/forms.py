a0af605b5940df40a95ea6b6185ed6bd15af2b7c
prosper-20 <edwardprosper001@gmail.com>
2021-12-28 00:52:46 +0100

    updates

--- Order/forms.py
+++ Order/forms.py
@@ -1,13 +1,25 @@
 from django import forms
-from django.countries.fields import CountryField 
+from django.utils.regex_helper import Choice
+from django_countries.fields import CountryField 
+
+PAYMENT_CHOICES = (
+    ("S", "Stripe"),
+    ("P", "PayPal")
+)
 
 
 class CheckoutForm(forms.Form):
-    street_address = forms.CharField()
-    appartment_address = forms.CharField(required=False)
-    country = CountryField(blank_label='(select country')
+    street_address = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': '1234 Main St'
+    }))
+    appartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
+        'placeholder': 'Apartment or Suite'}
+        ))
+    country = CountryField(blank_label='(select country').formfield(attrs={
+        'class': "custom-select d-block w-100"
+    })
     zip = forms.CharField()
     same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
     save_info = forms.BooleanField(widget=forms.CheckboxInput())
-    payment_option = forms.BooleanField(widget=forms.RadioSelect)
+    payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
 
