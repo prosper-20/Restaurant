5126536af96acfbcd52c336093895cbc9f7365ba
prosper-20 <edwardprosper001@gmail.com>
2021-12-28 20:39:25 +0100

    updates

--- Order/forms.py
+++ Order/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils.regex_helper import Choice
 from django_countries.fields import CountryField 
+from django_countries.widgets import CountrySelectWidget
+
 
 PAYMENT_CHOICES = (
     ("S", "Stripe"),
@@ -15,10 +17,13 @@ class CheckoutForm(forms.Form):
     appartment_address = forms.CharField(required=False, widget=forms.TextInput(attrs={
         'placeholder': 'Apartment or Suite'}
         ))
-    country = CountryField(blank_label='(select country').formfield(attrs={
-        'class': "custom-select d-block w-100"
-    })
-    zip = forms.CharField()
+    country = CountryField(blank_label='(select country').formfield(
+        widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100'
+        }))
+    zip = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
     same_billing_address = forms.BooleanField(required=False)
     save_info = forms.BooleanField(required=False)
     payment_option = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
