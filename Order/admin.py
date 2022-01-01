709261aa904d293f047a643c2f22baf219923af7
prosper-20 <edwardprosper001@gmail.com>
2022-01-01 15:16:43 +0100

    updates

--- Order/admin.py
+++ Order/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderItem, Item, Address, Payment, Coupon, Refund
+from .models import Order, OrderItem, Item, Address, Payment, Coupon, Refund, UserProfile
 
 def make_refund_accepted(modeladmin, request, queryset):
     queryset.update(refund_requested=False, refund_granted=True)
@@ -61,3 +61,4 @@ admin.site.register(Address, AddressAdmin)
 admin.site.register(Coupon)
 admin.site.register(Payment)
 admin.site.register(Refund)
+admin.site.register(UserProfile)
