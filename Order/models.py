d434315fce5c85fde78513f0acb36778ece05042
prosper-20 <edwardprosper001@gmail.com>
2022-01-01 10:25:57 +0100

    updates

--- Order/models.py
+++ Order/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.urls import reverse
+from django.db.models.signals import post_save
 from django_countries.fields import CountryField
 
 CATEGORY_CHOICES = (
@@ -22,6 +23,11 @@ ADDRESS_CHOICES = (
     ('S', 'Shipping'),
     
 )
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
+    one_click_purchasing = models.BooleanField()
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
@@ -154,3 +160,11 @@ class Refund(models.Model):
 
     def __str__(self):
         return f"{self.pk}"
+
+
+def userprofile_receiver(sender, instance, created, *args, **kwargs):
+    if created:
+        userprofile = UserProfile.objects.create(user=instance)
+
+post_save.connect(userprofile_receiver, sender=settings.AUTH_USER_MODEL)
+
