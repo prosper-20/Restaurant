31fc404b0f5dbc92db85de890221d564aabeabd8
prosper-20 <edwardprosper001@gmail.com>
2022-01-12 17:31:19 +0100

    updates

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,9 @@ from django.views.generic import ListView
 from Order.models import Item
 
 
-class Blog_Home(ListView):
-    model = Item
-    template_name = "blog/food-index.html"
-    context_object_name = 'items'
+def blog_home(request):
+    items = Item.objects.all()[1:3]
+    context = {
+        "items": items,
+    }
+    return render(request, "blog/food-index.html", context)
