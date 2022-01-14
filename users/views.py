5eb429a35b10ce69c5d816e4e7095b5e083c8a41
prosper-20 <edwardprosper001@gmail.com>
2022-01-14 16:43:55 +0100

    updates

--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm, ProfileUpdateForm, UserUpdateForm
+from django.contrib.auth.models import User
+from django.conf import settings
 
 
 def register(request):
@@ -14,6 +16,36 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, "users/register.html", {"form": form})
+
+def register(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.info(request, 'Huff ,Username already exist')
+            return redirect("register")
+        elif User.objects.filter(email=email).exists():
+            messages.info(request, 'Come On, Email was already Taken !')
+            return redirect("register")
+        else:
+            user = User.objects.create_user(
+                username=username, password=password, email=email)
+            mydict = {'username': username}
+            user.save()
+            html_template = 'register_email.html'
+            html_message = render_to_string(html_template, context=mydict)
+            subject = 'Welcome to Service-Verse'
+            email_from = settings.EMAIL_HOST_USER
+            recipient_list = [email]
+            message = EmailMessage(subject, html_message,
+                                   email_from, recipient_list)
+            message.content_subtype = 'html'
+            message.send()
+            return redirect("success")
+    else:
+        return render(request, 'register.html')
 
 
 
