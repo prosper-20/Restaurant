b907d5a20a5c0dbaa2c0e9c9bf0bf8de69f04067
prosper-20 <edwardprosper001@gmail.com>
2022-02-15 06:41:58 +0100

    updates

--- users/signals.py
+++ users/signals.py
@@ -9,7 +9,7 @@ from django.db.models.signals import post_save
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        userprofile = UserProfile.objects.create(user=instance)
+        UserProfile.objects.create(user=instance)
 
 
 @receiver(post_save, sender=User)
