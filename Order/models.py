14fa2bf7da0473197d4a8e60cdc74a5de8721c99
prosper-20 <edwardprosper001@gmail.com>
2022-02-23 01:01:23 +0100

    updates

--- Order/models.py
+++ Order/models.py
@@ -27,7 +27,8 @@ ADDRESS_CHOICES = (
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
-    one_click_purchasing = models.BooleanField()
+    # You added null = True
+    one_click_purchasing = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.username
