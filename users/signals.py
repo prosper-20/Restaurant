6634e051824c9f0d41960be20c1dd791529d089f
prosper-20 <edwardprosper001@gmail.com>
2022-02-23 07:07:38 +0100

    updates

--- users/signals.py
+++ users/signals.py
@@ -1,4 +1,5 @@
 import imp
+from django.conf import settings
 from django.db.models import signals
 from django.dispatch import receiver
 from django.contrib.auth.models import User
@@ -6,12 +7,14 @@ from Order.models import UserProfile
 from django.db.models.signals import post_save
 
 
-@receiver(post_save, sender=User)
+# @receiver(post_save, sender=User)
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_userprofile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def save_userprofile(sender, instance, **kwargs):
-    instance.profile.save()
+    instance.userprofile.save()
