7697db7a8ee808942cb475afcb9914ccb28c1b00
prosper-20 <edwardprosper001@gmail.com>
2022-03-08 23:38:24 +0100

    updates

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,22 @@ from typing import List
 from django.shortcuts import render
 from django.views.generic import ListView
 from Order.models import Item
+from .models import Post
 
 
-def blog_home(request):
-    items = Item.objects.all()[0:12]
-    context = {
-        "items": items,
-    }
-    return render(request, "blog/index.html", context)
+# def blog_home(request):
+#     items = Item.objects.all()[0:12]
+#     context = {
+#         "items": items,
+#     }
+#     return render(request, "blog/index.html", context)
+
+
+class HomeView(ListView):
+    model = Post
+    context_object_name = "posts"
+    template_name = "blog/index.html"
+    ordering = ["-date_posted"]
 
 def blog_about(request):
     return render(request, "blog/food-single.html")
