27e25e2639ab31b1d9af79503e2aecb0cc085895
prosper-20 <edwardprosper001@gmail.com>
2022-03-08 23:25:49 +0100

    updates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+CATEGORY_CHOICES = (
+    ('S', 'Snacks'),
+    ('E', 'Entre'),
+    ('D', 'Drink'),
+    ('A', 'Appetizer'),
+    ('MC', 'Main Course')
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(help_text="Be Expressive")
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=3)
+    image = models.ImageField()
+    slug = models.SlugField()
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("product", kwargs={
+            'slug': self.slug
+        })
